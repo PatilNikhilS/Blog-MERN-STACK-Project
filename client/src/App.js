87810276d06aab63868ae2de4722051a7eed33d9
Nikhil Suresh Patil <89438683+PatilNikhilS@users.noreply.github.com>
2023-04-14 10:18:21 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,72 +1,19 @@
 import "./App.css";
+import Layout from "./Layout";
+import { Route, Routes } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">
-          MyBlog
-        </a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img src="https://s.yimg.com/ny/api/res/1.2/4fzVesFNC7DcsQ8zeEE2Jg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/en/ap_finance_articles_694/567b0edf63e1359beed78ab43157fece"></img>
-        </div>
-        <div className="texts">
-          <h2>U.S. Adds A Healthy 236,000 Jobs Despite Fed's Rate Hikes</h2>
-          <p className="info">
-            <a className="author">Nikhil Patil</a>
-            <time>2023-03-31 12:30</time>
-          </p>
-          <p className="summary">
-            America's employers added a solid 236,000 jobs in March, suggesting
-            that the economy remains on solid footing despite the nine interest
-            rate hikes the Federal Reserve has imposed over the past year in its
-            drive to tame inflation.
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://s.yimg.com/ny/api/res/1.2/4fzVesFNC7DcsQ8zeEE2Jg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/en/ap_finance_articles_694/567b0edf63e1359beed78ab43157fece"></img>
-        </div>
-        <div className="texts">
-          <h2>U.S. Adds A Healthy 236,000 Jobs Despite Fed's Rate Hikes</h2>
-          <p className="info">
-            <a className="author">Nikhil Patil</a>
-            <time>2023-03-31 12:30</time>
-          </p>
-          <p className="summary">
-            America's employers added a solid 236,000 jobs in March, suggesting
-            that the economy remains on solid footing despite the nine interest
-            rate hikes the Federal Reserve has imposed over the past year in its
-            drive to tame inflation.
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://s.yimg.com/ny/api/res/1.2/4fzVesFNC7DcsQ8zeEE2Jg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/en/ap_finance_articles_694/567b0edf63e1359beed78ab43157fece"></img>
-        </div>
-        <div className="texts">
-          <h2>U.S. Adds A Healthy 236,000 Jobs Despite Fed's Rate Hikes</h2>
-          <p className="info">
-            <a className="author">Nikhil Patil</a>
-            <time>2023-03-31 12:30</time>
-          </p>
-          <p className="summary">
-            America's employers added a solid 236,000 jobs in March, suggesting
-            that the economy remains on solid footing despite the nine interest
-            rate hikes the Federal Reserve has imposed over the past year in its
-            drive to tame inflation.
-          </p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 }
 
